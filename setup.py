7c4a7ba0067c1c2ebb66b5b5e3d37143d88af968
ddddddeon <opfunk@gmail.com>
2024-02-10 15:21:36 -0500

    add requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="gen",
     version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=requirements,
     entry_points={"console_scripts": ["gen = gen.main:main"]},
 )
