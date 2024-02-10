14ca681288b8e53ef0248f620ca2ea5ac0a61585
ddddddeon <opfunk@gmail.com>
2024-02-10 15:15:56 -0500

    add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# pygen 
+Python implementation of (gen)[https://github.com/ddddddeon/gen]: a tool to generate project boilerplate, with support for various languages
+
+## Installation
+```sh
+git clone https://github.com/ddddddeon/pygen
+pip install .
+```
+
+## Usage
+```sh
+gen python my-project
+gen rust my-project --lib
+```
--- gen/util.py
+++ gen/util.py
@@ -6,14 +6,15 @@ def parse_args() -> tuple[Lang, str, ProjectType]:
     parser = argparse.ArgumentParser()
     parser.add_argument("lang")
     parser.add_argument("name")
-    parser.add_argument("proj_type", nargs="?", default="bin")
+    parser.add_argument("-l", "--lib", action="store_true")
+
     args = parser.parse_args()
 
     if not (args.lang and args.name):
-        print("Usage: pygen <lang> <name> [-l]")
+        print("Usage: pygen <lang> <name> [-l | --lib]")
         exit(1)
 
     lang = Lang(args.lang)
-    proj_type = ProjectType(args.proj_type)
+    proj_type = ProjectType.LIBRARY if args.lib else ProjectType.EXECUTABLE
 
     return lang, args.name, proj_type
