77540c43469295231744fec4f41efcbce08e8b20
ddddddeon <opfunk@gmail.com>
2024-08-10 13:42:02 -0400

    tweak setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 setup(
     name="gen",
     version="0.0.1",
-    packages=find_packages(),
+    packages=find_namespace_packages(),
     include_package_data=True,
     install_requires=requirements,
     entry_points={"console_scripts": ["gen = gen.main:main"]},
