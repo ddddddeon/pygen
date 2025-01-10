5831db93936964531b615160dd4cc6e1f38bf8dc
ddddddeon <opfunk@gmail.com>
2025-01-10 17:08:15 -0500

    Use cargo fmt for formatting

--- gen/main.py
+++ gen/main.py
@@ -1,10 +1,13 @@
 import os
 from gen.util import parse_args
 from gen.project import Project
-
+from gen.project import ProjectType
 
 def main():
     lang, name, proj_type = parse_args()
+    if not proj_type:
+        proj_type = ProjectType.EXECUTABLE
+        
     project = Project(
         lang=lang,
         name=name,
